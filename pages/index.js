8b04e0b7387f7b5ffe7fa14aa06a22c54e6a77bc
paimtarga <paimtarga@penguin>
2021-03-09 00:07:07 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
 import Link from 'next/link';
-<html>
-    <head>
-        <link
-            href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
-            rel="stylesheet"
-        />
-    </head>
-</html>
-var Latex = require('react-latex');
 function Home() {
     return <div>
-        <h2>Página Inicial <Latex>$\frac{1}{2}$</Latex></h2> 
+        <h2>Página Inicial</h2> 
 
         <Link href="/Mecânica">
             <a>Mecânica</a>
